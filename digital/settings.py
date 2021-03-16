e81a8ee251b5ad08e709d0394291c6f0f37d0ab8
Nikhil <nikshh.kumar@gmail.com>
2021-03-16 23:28:19 +0530

    requirement1 error

--- digital/settings.py
+++ digital/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoise.Middleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'digital.urls'
